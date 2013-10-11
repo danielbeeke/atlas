c5848f12ad0f405a167c1ebbdfcdb37e4b404029
danielbeeke <daniel.beek.e@gmail.com>
2013-10-11 22:53:01 +0200

    updates

--- modules/features/a_location_photos/js/a_location_photos.app.js
+++ modules/features/a_location_photos/js/a_location_photos.app.js
@@ -11,16 +11,41 @@
             mode:'horizontal',
             grabCursor: true,
             slidesPerView: 'auto',
-            resistance: false
+            resistance: false,
+            preventLinks: true
           })
         }
 
+        $('.swiper-image').once().click(function () {
+            var that = this;
+
+            $('#photo').remove()
+
+            var singlePhoto = twigloader.get('photo', {
+              photo: $(that).attr('data-large'),
+              title: $(that).attr('title'),
+            })
+
+            if ($(that).attr('title')) {
+              var title = $(that).attr('title')
+            }
+            else {
+              var title = 'No title';
+            }
+
+            var photoModal = twigloader.get('modal', {
+              id: 'photo',
+              content: singlePhoto,
+              title: title
+            })
+
+            $('body').append(photoModal)
+
+            $('#photo').modal('show')
+
+        });
+
       })
-
-      $(document).on('hide.bs.plate', function () {
-
-      })
-
 
     }
   };
